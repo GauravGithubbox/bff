a7e38ac7cc3592e21a53fabbffc6ec7abd60b43e
GauravGithubbox <Gaurav.srivastav@knoldus.com>
2022-07-29 14:05:18 +0530

    Initial commit

---
+++ src/main/java/ca/loblaw/bff/LobLawBffApplication.java
@@ -0,0 +1,13 @@
+package ca.loblaw.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LobLawBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LobLawBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/loblaw/bff/LobLawBffApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.loblaw.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobLawBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
